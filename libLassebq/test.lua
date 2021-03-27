673d4c714b62c467c0dc406367828e1fd70e963e
Nikita Krapivin <alienoom@yandex.ru>
2021-03-27 17:36:53 +0500

    Finalize 2.1 source tree

--- libLassebq/test.lua
+++ libLassebq/test.lua
@@ -2,6 +2,7 @@
 
 local obj_libLassebq = { }
 local player_hurt = { }
+local generate_hash = { }
 
 function getDateTime() -- a helper to get formatted datetime from GM
 	return GML_date_datetime_string(GML_date_current_datetime())
@@ -10,6 +11,7 @@ end
 function obj_libLassebq.PressF5(_pSelf, _pOther)
 	GML_game_save("temp00")
 	GML_window_set_caption("Saved, time: " .. getDateTime())
+	local x = GMLua_inst(_pSelf.id)
 end
 
 function obj_libLassebq.PressF6(_pSelf, _pOther)
@@ -17,37 +19,4 @@ function obj_libLassebq.PressF6(_pSelf, _pOther)
 	GML_window_set_caption("Loaded, time: " .. getDateTime())
 end
 
---[[
-	hooker routine:
-	if Before defined then
-		r = call Before
-		if Before returned
-			return r
-		else
-			r = call Original script
-			if After defined
-				r = call After and provide Original r
-				return r
-			end
-		end
-	end
-
-	so by returning from Before, we prevent the original script from executing!
-]]--
-
-function player_hurt.Before(_pSelf, _pOther, _args)
-	--[[ You can't die, you can't die, you can't die, you can't die, you can't die
-	     that would suck, right? sometimes you just *have* to die, your life's over
-	     but no, you are still alive, watching how your close relatives and friends die
-	     pretty painful, ain't it?
-	     welp, I don't have any IRL friends, so not really a problem for me.
-	     UHHHHHHHHHHHHHHHH NEVERMIND I'VE GONE TOO FAR.
-	     oh, btw, `_args` is an array with the arguments. ]]--
-	return 0 -- ????????
-end
-
-function player_hurt.After(_pSelf, _pOther, _args, _ret)
-	return _ret -- this won't be executed anyway, you don't even need to return
-end
-
-return { obj_libLassebq=obj_libLassebq, player_hurt=player_hurt }
+return { obj_libLassebq=obj_libLassebq }
